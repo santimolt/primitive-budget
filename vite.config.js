71a81fa57ef5c07e5c7599d729f7754d06efe04f
Santiago Moltedo <77304038+santimolt@users.noreply.github.com>
2025-11-25 02:47:27 -0300

    Fixes for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Descomenta la siguiente línea si vas a desplegar en GitHub Pages
-  // base: '/budget-primitivo/',
+  base: '/primitive-budget/',
 })
 
